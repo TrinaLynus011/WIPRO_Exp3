095f03c5536fe6b43ee996816889079113465433
Trina Lynus <trinalynus@gmail.com>
2026-09-07 23:35:21 +0530

    Add AccessTest class to test foundation package Q2.1

---
+++ AccessTest.java
@@ -0,0 +1,13 @@
+package test2;
+
+import test.foundation;
+
+public class AccessTest {
+
+    public static void main(String[] args) {
+
+        foundation obj = new foundation();
+
+        System.out.println(obj.var4);
+    }
+}
